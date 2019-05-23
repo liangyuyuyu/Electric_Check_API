9cb27fa1f2157bc525e2e53868314f04a320f66f
liangyuyu <1391848245@qq.com>
2019-05-23 22:13:08 +0800

    解决post请求不成功

--- Electric_Check/Global.asax.cs
+++ Electric_Check/Global.asax.cs
@@ -19,5 +19,12 @@ namespace Electric_Check
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void Application_BeginRequest()
+        {
+            if (Request.Headers.AllKeys.Contains("Origin") && Request.HttpMethod == "OPTIONS")
+            {
+                Response.End();
+            }
+        }
     }
 }
